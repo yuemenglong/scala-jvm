69f626be195850c35fed50478f382c10682e8040
yuemenglong <yuemenglong@126.com>
2018-02-13 10:25:16 +0800

    add: 重构OpLoad

--- src/main/scala/io/github/yuemenglong/jvm/op/OpLoad.scala
+++ src/main/scala/io/github/yuemenglong/jvm/op/OpLoad.scala
@@ -10,46 +10,29 @@ import scala.reflect.{ClassTag, classTag}
   */
 object OpLoad {
   def load(reader: StreamReader, cf: ClassFile, method: MethodInfo, lineNo: Int, code: Int): Op = {
-    val index = code match {
-      case c if 0x15 <= c && c <= 0x19 => reader.readByte()
-      case c if 0x1A <= c && c <= 0x1D => c - 0x1A
-      case c if 0x1E <= c && c <= 0x21 => c - 0x1E
-      case c if 0x22 <= c && c <= 0x25 => c - 0x22
-      case c if 0x26 <= c && c <= 0x29 => c - 0x26
-      case c if 0x2A <= c && c <= 0x2D => c - 0x2A
-    }
     code match {
-      case 0x15 => new OpLoad[Int](reader, cf, method, lineNo, code, index.toByte)
-      case 0x16 => new OpLoad[Long](reader, cf, method, lineNo, code, index.toByte)
-      case 0x17 => new OpLoad[Float](reader, cf, method, lineNo, code, index.toByte)
-      case 0x18 => new OpLoad[Double](reader, cf, method, lineNo, code, index.toByte)
-      case 0x19 => new OpLoad[AnyRef](reader, cf, method, lineNo, code, index.toByte)
-      case c if 0x1A <= c && c <= 0x1D => new OpLoad[Int](reader, cf, method, lineNo, code, index.toByte)
-      case c if 0x1E <= c && c <= 0x21 => new OpLoad[Long](reader, cf, method, lineNo, code, index.toByte)
-      case c if 0x22 <= c && c <= 0x25 => new OpLoad[Float](reader, cf, method, lineNo, code, index.toByte)
-      case c if 0x26 <= c && c <= 0x29 => new OpLoad[Double](reader, cf, method, lineNo, code, index.toByte)
-      case c if 0x2A <= c && c <= 0x2D => new OpLoad[AnyRef](reader, cf, method, lineNo, code, index.toByte)
+      case c if 0x15 <= c && c <= 0x2D => new OpLoad(reader, cf, method, lineNo, code)
     }
   }
 }
 
-class OpLoad[T: ClassTag](reader: StreamReader,
-                          override val cf: ClassFile,
-                          override val method: MethodInfo,
-                          val lineNo: Int,
-                          val opCode: Int,
-                          val index: Byte) extends Op {
-  require(0x15 <= opCode && opCode <= 0x2D)
+class OpLoad(reader: StreamReader,
+             override val cf: ClassFile,
+             override val method: MethodInfo,
+             val lineNo: Int,
+             val opCode: Int
+            ) extends Op {
+  val index = opCode match {
+    case c if 0x15 <= c && c <= 0x19 => reader.readByte().toInt
+    case c if 0x1A <= c && c <= 0x2D => (opCode - 0x1A) % 4 + 1
+  }
 
   override val opName = {
-    val prefix = classTag[T].runtimeClass match {
-      case Types.classOfInt => "i"
-      case Types.classOfLong => "l"
-      case Types.classOfFloat => "f"
-      case Types.classOfDouble => "d"
-      case Types.classOfRef => "a"
+    val prefix = opCode match {
+      case c if 0x15 <= c && c <= 0x19 => "ilfda".charAt(opCode - 0x15)
+      case c if 0x1A <= c && c <= 0x2D => "ilfda".charAt((opCode - 0x1A) / 4)
     }
-    s"${prefix}load_${index}"
+    s"${prefix}load ${index}"
   }
 
   override def proc(ctx: RtCtx): Unit = ???
