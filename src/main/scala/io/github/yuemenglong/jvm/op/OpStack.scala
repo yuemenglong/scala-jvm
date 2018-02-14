a008120130a9b29727ce80b4d3a6014143bb5976
yuemenglong <yuemenglong@126.com>
2018-02-14 10:16:44 +0800

    add: OpStack

--- src/main/scala/io/github/yuemenglong/jvm/op/OpStack.scala
+++ src/main/scala/io/github/yuemenglong/jvm/op/OpStack.scala
@@ -9,15 +9,40 @@ import scala.reflect.{ClassTag, classTag}
 /**
   * Created by <yuemenglong@126.com> on 2018/2/12.
   */
-object OpDup {
+object OpStack {
   def load(reader: StreamReader, cf: ClassFile, method: MethodInfo, lineNo: Int, code: Int): Op = {
     code match {
+      case c if 0x57 <= c && c <= 0x58 => new OpPop(reader, cf, method, lineNo, code)
       case c if 0x59 <= c && c <= 0x5B => new OpDup(reader, cf, method, lineNo, code)
       case c if 0x5C <= c && c <= 0x5E => new OpDup2(reader, cf, method, lineNo, code)
-      case 0x5F => new OpSwap(reader, cf, method, lineNo, code)
+      case c if 0x5F <= c && c <= 0x5F => new OpSwap(reader, cf, method, lineNo, code)
     }
   }
 }
+
+class OpPop(reader: StreamReader,
+            override val cf: ClassFile,
+            override val method: MethodInfo,
+            val lineNo: Int,
+            val opCode: Int,
+           ) extends Op {
+  override val opName = {
+    opCode match {
+      case 0x57 => "pop"
+      case 0x58 => "pop2"
+    }
+  }
+
+  override def proc(ctx: ThreadCtx): Unit = {
+    val top = ctx.pop()
+    val is8Byte = top.isInstanceOf[Long] || top.isInstanceOf[Double]
+    opCode match {
+      case 0x57 => require(!is8Byte)
+      case 0x58 => require(is8Byte)
+    }
+  }
+}
+
 
 class OpDup(val reader: StreamReader,
             override val cf: ClassFile,
